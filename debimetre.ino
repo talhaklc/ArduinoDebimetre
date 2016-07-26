e878cfcb283cda8418b5db132b497bddd662c7b6
Talha KILIÇ <talha-kilic@hotmail.com>
2016-07-26 17:39:16 +0300

    Add files via upload

--- debimetre.ino
+++ debimetre.ino
@@ -1,6 +1,6 @@
 volatile int flow_frequency; // Sensor palsı
 unsigned int l_hour; // Hesaplanan Litre/saat
-unsigned char flowsensor = 2; // Sensor Giriş Pini
+unsigned char flowsensor = 18; // Sensor Giriş Pini
 unsigned long currentTime;
 unsigned long cloopTime;
 int i=0; int yeni=0; int eski=0;
@@ -36,21 +36,14 @@ void setup()
 }
 
 void loop ()
-{
- Serial.println(emmemiktari);
-    sondurum=sayac-yenisayac;
-   if(sondurum>30){
-    emmemiktari++;
-   }
-   yenisayac=sayac;
- delay(500); 
+{ 
  }
 void debi(){
    currentTime = millis();
    // Her saniye hesaplar ve litre/saat yazdırır.
    if(currentTime >= (cloopTime + 1000))
    {
-      flowRate = ((1000.0 / (millis() - cloopTime)) * flow_frequency) / 7.5;
+      flowRate = ((1000.0 / (millis() - cloopTime)) * flow_frequency) / 4.5;
       cloopTime = millis(); // cloopTime güncelle,yenile 
     
       flowMilliLitres = (flowRate / 60)*1000 ;
@@ -60,7 +53,7 @@ void debi(){
       yeni=flow_frequency+totalMilliLitres;
      
      
-      l_hour = (flow_frequency * 60 / 7.5); // (Darbe frekansı x 60 dk) / 7.5Q = Akış Hızı Litre/saat
+      l_hour = (flow_frequency * 60 / 15); // (Darbe frekansı x 60 dk) / 7.5Q = Akış Hızı Litre/saat
       flow_frequency = 0; // Sayacı Sıfırla..
       
       //Serial.print("  Sivi Akis Hizi: "); 
@@ -71,9 +64,9 @@ void debi(){
      // Serial.print(flowMilliLitres, DEC); // Yazdır litre/saat
      // Serial.println(" mL/saniye \n");
       
-     // Serial.print("  Toplam Sivi: "); 
-     // Serial.print(totalMilliLitres);
-//Serial.println("mL \n"); 
+     Serial.print("  Toplam Sivi: "); 
+      Serial.print(totalMilliLitres);
+Serial.println("mL \n"); 
              
              i++;
 
